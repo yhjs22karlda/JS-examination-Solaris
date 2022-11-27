c76c8601e9133de3eef0e873942718b089e56eb1
yhjs22karlda <yhjs22.karlda@folkuniversitetet.nu>
2022-11-27 18:18:48 +0100

    v1.0

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const BASE_URL = "https://fathomless-shelf-54969.herokuapp.com";
-const planets = await getPlanets();
-plotPlanets(planets);
 // fetch ------------------------------------------------------------------
+const BASE_URL = "https://fathomless-shelf-54969.herokuapp.com";
 
 async function getKey() {
-    const response = await fetch(`${BASE_URL}/keys`, {method: "POST"});
+    const response = await fetch(`${BASE_URL}/keys`, {
+        method: "POST"
+    });
     const data = await response.json()
     return data.key
 }
@@ -13,7 +13,9 @@ async function getPlanets() {
     const planetColors = ["#fdce3f", "#888888", "#e7cdcd", "#428ed4", "#ef5f5f", "#e29468", "#c7aa72", "#c9d4f1", "#7a91a7"]
     const key = await getKey();
     const response = await fetch(`${BASE_URL}/bodies`, {
-        headers: {"x-zocom":key}
+        headers: {
+            "x-zocom": key
+        }
     })
     const data = await response.json();
     data.bodies.forEach((element, index) => {
@@ -23,28 +25,29 @@ async function getPlanets() {
     return data.bodies;
 }
 // plot-------------------------------------------------------------------
-
 function plotPlanets(planets) {
-    let earthCirc = planets[3].circumference;
+    let scaleFactor = planets[3].circumference;
     const planetsElem = document.querySelector(".planets__planets");
-    for(let i = 1; i < planets.length; i++) {
+    for (let i = 1; i < planets.length; i++) {
         const article = document.createElement("article");
-        article.id = `planet-${planets[i].name.toLowerCase()}`;
-        article.style.width = planets[i].circumference/earthCirc*25 +"px";
+        article.style.width = planets[i].circumference / scaleFactor * 25 + "px";
         article.style.aspectRatio = "1";
         article.style.backgroundColor = planets[i].color;
         article.setAttribute("title", planets[i].name)
         article.setAttribute("data-index", i)
         article.classList.add("planets__planet");
+        if (i === 6) makeRings(article);
         planetsElem.append(article);
     }
-}
 
-function clickedOnPlanet(event) {
-    const planetIndex = event.target.dataset.index;
-    toggleScreen();
-    showPlanetInfo(planets[planetIndex])
+    function makeRings(saturnus) {
+        saturnus.style.position = "relative"
+        const rings = document.createElement("article");
+        rings.id = "saturnus-rings";
+        saturnus.append(rings);
+    }
 }
+// show info---------------------------------------------------------------
 function showPlanetInfo(planet) {
     const planetinfoH1 = document.querySelector(".planetinfo h1");
     planetinfoH1.textContent = planet.name.toUpperCase();
@@ -65,12 +68,11 @@ function showPlanetInfo(planet) {
     const planetinfoBack = document.querySelector(".planetinfo__back");
     planetinfoBack.textContent = "<-- Back to Planets";
     makeBackground(planet.color);
-}
 
-function makeBackground(color) {
-    const background = document.querySelector(".planetinfo");
-    background.style.background =
-        `radial-gradient(circle at -34%,
+    function makeBackground(color) {
+        const background = document.querySelector(".planetinfo");
+        background.style.background =
+            `radial-gradient(circle at -34%,
             ${color}ff 30%,
             ${color}44 30%,
             ${color}44 32%,
@@ -80,6 +82,15 @@ function makeBackground(color) {
         ),
         url(stars.png),
         linear-gradient(-90deg,#180b22, #0e194a)`
+    }
+}
+const planets = await getPlanets();
+plotPlanets(planets);
+// eventliteners----------------------------------------------
+function clickedOnPlanet(event) {
+    const planetIndex = event.target.dataset.index;
+    toggleScreen();
+    showPlanetInfo(planets[planetIndex])
 }
 
 function toggleScreen() {
@@ -88,7 +99,7 @@ function toggleScreen() {
     planetInfoElem.classList.toggle("fadeIn")
     planetElem.classList.toggle("fadeOut")
 }
-// eventliteners----------------------------------------------
+
 const back = document.querySelector(".planetinfo__back");
 back.addEventListener("click", toggleScreen);
 
@@ -96,29 +107,20 @@ const theSunElem = document.querySelector(".planets__solen");
 theSunElem.addEventListener("click", clickedOnPlanet);
 
 const planetsElems = document.querySelectorAll(".planets__planet");
-for(let planet of planetsElems) {
+for (let planet of planetsElems) {
     planet.addEventListener("click", clickedOnPlanet);
     planet.addEventListener("mouseenter", (e) => {
-        // e.target.style.transform = `scale(${e.target.clientHeight * 0.02})`;
-        // e.target.style.position = "relative"
+        const height = e.target.clientHeight
+        e.target.style.transform = `scale(${(height + 5) / height})`;
+        // e.target.style.outline = "2px solid black"
         // let width = parseInt(e.target.style.width);
         // e.target.style.width = width + 5 + "px"
     });
     planet.addEventListener("mouseleave", (e) => {
         e.target.style.transform = "scale(1)";
+        // e.target.style.outline = "none"
         // e.target.style.position = "unset";
         // let width = parseInt(e.target.style.width);
         // e.target.style.width = width - 5 + "px"
     });
-
 }
-
-// const canvas = document.querySelector("#canvas");
-// const ctx = canvas.getContext("2d");
-// ctx.beginPath();
-// ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-// ctx.stroke();
-// canvas.style.position = "absolute";
-// canvas.style.width = "100%";
-// canvas.style.height = "100%";
-
